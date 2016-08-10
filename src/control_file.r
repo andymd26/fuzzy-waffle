749b0fa0ab9d3828b277f58f5889764216878219
Andrew Blohm <andymd26@umd.edu>
2016-08-10 15:19:04 -0400

    Created a source file for the repository
    All of the code can be run from this central file.

---
+++ src/control_file.r
@@ -0,0 +1,18 @@
+install.packages("R.utils")
+install.packages("tidyr")
+install.packages("dplyr")
+install.packages('data.table')
+options(scipen=999)
+require(R.utils)
+require(dplyr)
+require(tidyr)
+require(data.table)
+
+path_code = "C:/Users/bloh356/Documents/GitHub/fuzzy-waffle/src/"
+path_data = "C:/Users/bloh356/Documents/GitHub/fuzzy-waffle/data/"
+#path_data = "/Users/bloh356/Documents/fuzzy-waffle/data/"
+# Local path to the cloned data and code folders in the repository
+
+source(paste0(path_code,"mapping_860_overnight_R.r"))
+source(paste0(path_code, "ca_almanac_R.r"))
+       
