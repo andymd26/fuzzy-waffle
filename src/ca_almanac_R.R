1948656afa4be156546f9d39e7209de781d77c39
Andrew Blohm <andymd26@umd.edu>
2016-08-11 15:09:46 -0400

    Minor change

--- src/ca_almanac_R.R
+++ src/ca_almanac_R.R
@@ -14,14 +14,14 @@ ca.elec.almanac = ca.elec.almanac %>%
   left_join(ca.mapping, by = "plant.id") %>%
   filter(year != 2015)
 
-cap.eia = read.table(paste0(path_data, "clean_capacity_eia_860_overnight_cost.txt.gz"), header=TRUE, sep ="\t")
+cap.eia = read.table(paste0(path_data, "mapping_860_overnight_R.txt.gz"), header=TRUE, sep ="\t")
 cap.eia = cap.eia %>%
   left_join(ca.elec.almanac, by = c("year", "plant_code")) %>%
   mutate(heat.rate = 1000*heat.rate) %>%
   # Converts units from MMbtu/MWh to btu/kWh (units favored by the EIA)
   mutate(heat_rate = ifelse(is.na(heat_rate) == FALSE, heat_rate, heat.rate))
 
-gz1 = gzfile(paste(path_data,"clean_capacity_eia_860_overnight_cost.txt.gz", sep=""), "w")
+gz1 = gzfile(paste(path_data,"ca_almanac_R.txt.gz", sep=""), "w")
 write.table(cap.eia, file = gz1, sep="\t",col.names = TRUE, row.names = FALSE)
 close(gz1)
 # Adds the heat rate information from the California energy almanac
