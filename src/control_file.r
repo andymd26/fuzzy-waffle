64810e94292ea43d9729f3d9fa333d72c786c677
Andrew Blohm <andymd26@umd.edu>
2016-08-17 08:29:32 -0400

    Source summary_file.r
    Added the summary_file.r file to the control document

--- src/control_file.r
+++ src/control_file.r
@@ -19,5 +19,6 @@ path_figure = "C:/Users/bloh356/Documents/GitHub/fuzzy-waffle/papers/figures/"
 source(paste0(path_code,"mapping_860_overnight_R.r"))
 # Warning messages occur because we are matching factors where not all of the factors are in both datasets (Ignore the warning)
 source(paste0(path_code, "ca_almanac_R.r"))
+source(paste0(path_code, "summary_files.r"))
 source(paste0(path_code, "figure.r"))
 
