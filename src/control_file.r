187764532303846994bafdc5425b637e2d473891
Andrew Blohm <andymd26@umd.edu>
2016-08-25 13:33:02 -0400

    Added source file

--- src/control_file.r
+++ src/control_file.r
@@ -20,5 +20,6 @@ source(paste0(path_code,"mapping_860_overnight_R.r"))
 # Warning messages occur because we are matching factors where not all of the factors are in both datasets (Ignore the warning)
 source(paste0(path_code, "ca_almanac_R.r"))
 source(paste0(path_code, "summary_files.r"))
+source(paste0(path_code, "overnight_cost.r"))
 source(paste0(path_code, "figure.r"))
 
