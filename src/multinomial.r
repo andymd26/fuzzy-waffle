7687e59a84cc32b5cdbba05cc29fea06bf070b5f
Andrew Blohm <andymd26@umd.edu>
2016-09-13 15:04:28 -0400

    We attempted to use the mlogit package in R but have had significant trouble in implementation. We are instead now pursuing a glm approach. The code at this point though is dedicated to the mlogit approach.

--- src/multinomial.r
+++ src/multinomial.r
@@ -1,11 +1,3 @@
-my_func = function(x, y, z){
-  a = categories[!categories$choice %in% x, ]
-  a['id'] = z
-  a['year'] = y
-  return(data.frame(a))
-}
-# This function adds all of the choice alternatives to the dataset (not just the option that was selected)
-
 data.raw = read.table(paste0(path_data, "processed.overnight.txt.gz"), header=TRUE, sep ="\t", as.is = TRUE)
 
 data.final = data.raw %>%
@@ -34,19 +26,30 @@ data.final = data.raw %>%
   # We don't have cost data for steam turbine plants that use natural gas (beyond 2000 they aren't a category in the AEO)
   mutate(adj.overnight = base.overnight/capacity.factor.avg) %>%
   mutate(choice = paste(overnight_category, fuel_1_general, sep = " ")) %>%
+  filter(choice != 'igcc coal') %>%
+  filter(choice != 'nuclear uranium') %>%
+  # Neither of these alternatives is ever selected in the mix, which seems to cause issues in the mlogit estimation
   arrange(overnight_category, fuel_1_general, year) %>%
   select(year, choice, overnight_category, fuel_1_general, capacity.factor.avg, capacity_mw, adj.fuel.price, variable.o.m, fixed.o.m, adj.overnight)
-  
+
 gz1 = gzfile(paste0(path_data,"data.final.txt.gz"), "w")
 write.table(data.final, file = gz1, sep="\t", col.names = TRUE, row.names = FALSE)
 close(gz1) 
 
 categories = unique(data.final[, c('overnight_category', 'fuel_1_general')]) %>%
-  mutate(choice = paste(categories$overnight_category,categories$fuel_1_general, sep=" ")) %>%
+  mutate(choice = paste(overnight_category, fuel_1_general, sep=" ")) %>%
   mutate(year = NA) %>%
   mutate(id = NA) %>%
   mutate(year = NA) %>%
   mutate(decision = FALSE)
+
+my_func = function(x, y, z){
+  a = categories[!categories$choice %in% x, ]
+  a['id'] = z
+  a['year'] = y
+  return(data.frame(a))
+}
+# This function adds all of the choice alternatives to the dataset (not just the option that was selected)
 
 capacity_1mw = data.final %>%
   select(year,overnight_category, fuel_1_general, choice, capacity_mw) %>%
@@ -60,88 +63,50 @@ df = capacity_1mw %>%
   ungroup() %>%
   bind_rows(capacity_1mw) %>%
   select(overnight_category, fuel_1_general, choice, year, id, decision) %>%
-  left_join(data.final, by = c('overnight_category', 'fuel_1_general', 'choice', 'year'))
-  
+  left_join(data.final, by = c('overnight_category', 'fuel_1_general', 'choice', 'year')) %>%
+  arrange(id, choice)
+
+gz1 = gzfile(paste0(path_data,"data.expanded.txt.gz"), "w")
+write.table(df, file = gz1, sep="\t", col.names = TRUE, row.names = FALSE)
+close(gz1) 
+
+df = read.table(paste0(path_data, "data.expanded.txt.gz"), header=TRUE, sep ="\t", as.is = TRUE)
 
 
+test = df %>%
+  filter(choice != 'igcc coal') %>%
+  filter(choice != 'nuclear uranium') %>%
+  arrange(id, choice)
 
-data = mlogit.data(data.final, shape = 'long', choice = 'choice', varying = 8:11, alt.levels = c(1,2), id = "year")
-capacity = mlogit.data(data.final, shape = 'long', varying = 8:11, choice = 'choice', 
-                       alt.var = c('adj.overnight','adj.fuel.price','variable.o.m','fixed.o.m'),
-                       alt.levels = c('coal coal','conventional combined cycle natural gas',"conventional combined cycle oil","conventional combustion turbine natural gas",
-                                      "conventional combustion turbine oil","geothermal geothermal","hydro water","igcc coal","nuclear uranium","photovoltaic solar",
-                                      "solar thermal solar","wind wind"))
+test.1 = mlogit.data(df, choice = 'decision', shape = 'long', chid.var = 'id',
+                alt.levels = c("coal coal", "conventional combined cycle natural gas", "conventional combined cycle oil",
+                               "conventional combustion turbine natural gas", "conventional combustion turbine oil",         
+                               "geothermal geothermal", "hydro water", "photovoltaic solar",
+                               "solar thermal solar", "wind wind"))
+test.1 = mlogit.data(test, choice = 'decision', shape = 'long', alt.var = 'choice')
+f = mFormula(decision ~ adj.overnight)
+#View(model.matrix(f, test.1))
 
+f.1 = mlogit(f, test.1, shape='long', alt.var='choice')
+f.1 = mlogit(decision ~ adj.overnight | id, data = test.1)
 
+summary(mlogit(decision ~ 0 | adj.overnight, data = test.1))
 
+test = df %>%
+  filter(year == 2014) %>%
+  mutate(new = paste(id, choice, sep='.')) 
 
-library("mlogit")
-data("HC", package = "mlogit")
-HC <- mlogit.data(HC, varying = c(2:8, 10:16), choice = "depvar", shape = "wide")
-cooling.modes <- index(HC)$alt %in% c(
-room.modes <- index(HC)$alt %in% c(
-  '
-  erc
-  '
-  ,
-  '
-  er
-  '
-)
-R> # installation / operating costs for cooling are constants,
-  R> # only relevant for mixed systems
-  R> HC$icca[!cooling.modes] <- 0
-R> HC$occa[!cooling.modes] <- 0
-R> # create income variables for two sets cooling and rooms
-  R> HC$inc.cooling <- HC$inc.room <- 0
-R> HC$inc.cooling[cooling.modes] <- HC$income[cooling.modes]
-R> HC$inc.room[room.modes] <- HC$income[room.modes]
-R> # create an intercet for cooling modes
-  R> HC$int.cooling <- as.numeric(cooling.modes)
-R> # estimate the model with only one nest elasticity
-  R> nl <- mlogit(depvar ~ ich + och +icca + occa + inc.room + inc.cooling + int.cooling | 0, HC,
-                  +              nests = list(cooling = c(
-                    '
-                    gcc
-                    '
-                    ,
-                    '
-                    ecc
-                    '
-                    ,
-                    '
-                    erc
-                    '
-                    ,
-                    '
-                    hpc
-                    '
-                  ),
-                  +              other = c(
-                    '
-                    gc
-                    '
-                    ,
-                    '
-                    ec
-                    '
-                    ,
-                    '
-                    er
-                    '
-                  )), un.nest.el = TRUE)
+test$id = as.character(test$id)
+test$choice = as.character(test$choice)
 
+data = mlogit.data(test, choice = 'decision', shape = 'wide', id.var = c('id','year'), alt.var = 'choice')
+data = data %>%
+  arrange(fuel_1_general)
 
-mutate(id.choice = NA) %>%
-  mutate(id.choice = replace(id.choice, choice == "coal coal", 1)) %>%
-  mutate(id.choice = replace(id.choice, choice == "conventional combined cycle natural gas", 2)) %>%
-  mutate(id.choice = replace(id.choice, choice == "conventional combined cycle oil", 3)) %>%
-  mutate(id.choice = replace(id.choice, choice == "conventional combustion turbine natural gas", 4)) %>%
-  mutate(id.choice = replace(id.choice, choice == "conventional combustion turbine oil", 5)) %>%
-  mutate(id.choice = replace(id.choice, choice == "geothermal geothermal", 6)) %>%
-  mutate(id.choice = replace(id.choice, choice == "hydro water", 7)) %>%
-  mutate(id.choice = replace(id.choice, choice == "igcc coal", 8)) %>%
-  mutate(id.choice = replace(id.choice, choice == "nuclear uranium", 9)) %>%
-  mutate(id.choice = replace(id.choice, choice == "photovoltaic solar", 10)) %>%
-  mutate(id.choice = replace(id.choice, choice == "solar thermal solar", 11)) %>%
-  mutate(id.choice = replace(id.choice, choice == "wind wind", 12)) %>%
+ml.capacity = mlogit(decision ~ adj.fuel.price + variable.o.m + fixed.o.m + adj.overnight | 0, df)
+
+nl.capacity = mlogit(decision ~ adj.fuel.price + variable.o.m + fixed.o.m + adj.overnight, data, panel = TRUE,
+                     nests = list(coal= c('coal coal', 'igcc coal'), oil = c('conventional combustion turbine oil', 'conventional combined cycle oil'),
+                                  ng = c('conventional combined cycle natural gas', 'conventional combustion turbine natural gas'),
+                                  geothermal = 'geothermal geothermal', nuclear = 'nuclear uranium', solar = c('solar thermal solar', 'photovoltaic solar'),
+                                  hydro = 'hydro water', wind = 'wind wind'), un.nest.el=TRUE)
